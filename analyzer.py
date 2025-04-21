d14938e721d8b734bc0d340c94df3ea7df8fd380
Mike Fischer <mike@theelectricrambler.com>
2025-04-21 14:23:58 -0600

    feat: Update imports and refactor response structure in analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,14 +1,15 @@
-from langchain_community.llms import Ollama
+from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import PromptTemplate
 from langchain_core.documents import Document
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import SentenceTransformersTokenTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 import datetime
 
-#from langchain_community.document_loaders import WebBaseLoader
+# from langchain_community.document_loaders import WebBaseLoader
+
 
 def get_response(content, question, model, systemprompt):
 
@@ -19,7 +20,7 @@ def get_response(content, question, model, systemprompt):
     except Exception as e:
         return str(e)
 
-    llm = Ollama(model=model)
+    llm = OllamaLLM(model=model)
     embeddings = OllamaEmbeddings(model=model)
     text_splitter = SentenceTransformersTokenTextSplitter()
     documents = text_splitter.split_documents(docs)
@@ -31,19 +32,21 @@ def get_response(content, question, model, systemprompt):
 
     retriever = vector.as_retriever()
     retrieval_chain = create_retrieval_chain(retriever, document_chain)
-    
+
     response = retrieval_chain.invoke({"input": question})
 
-    return(
-        {
-            'response': response["answer"],
-            'metadata': {
-                'start time': starttime, 
-                'stop time': datetime.datetime.now(), 
-                'model used': model,
-                'system prompt': systemprompt,
-                'question (\{input\})': question,
-                'context': content if len(content) < 100000 else content[:10000] + ' \n\n [rest hidden as too large]'
-            }
-        })
-
+    return {
+        "response": response["answer"],
+        "metadata": {
+            "start time": starttime,
+            "stop time": datetime.datetime.now(),
+            "model used": model,
+            "system prompt": systemprompt,
+            "question (\{input\})": question,
+            "context": (
+                content
+                if len(content) < 100000
+                else content[:10000] + " \n\n [rest hidden as too large]"
+            ),
+        },
+    }
